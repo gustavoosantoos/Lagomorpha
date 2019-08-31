6bfe2a150567924f387602719579936cdb513bd1
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-08-30 21:52:06 -0300

    Add Sample Web Project with background service

---
+++ Samples/LagomorphaTests/Handlers/ProductsHandler.cs
@@ -0,0 +1,29 @@
+﻿using Lagomorpha;
+using System;
+using System.Diagnostics;
+
+namespace LagomorphaTests.Handlers
+{
+    public class ProductsHandler
+    {
+        [QueueHandler("new-products")]
+        public void HandleNewProduct(NewProduct p)
+        {
+
+        }
+
+        [QueueHandler("remove-products")]
+        public void HandleRemoveProduct(Guid codigo)
+        {
+            Debug.WriteLine("=================== MENSAGEM RECEBIDA =================");
+            Debug.WriteLine(codigo);
+            Debug.WriteLine("");
+            Debug.WriteLine("");
+        }
+    }
+
+    public class NewProduct
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Samples/LagomorphaTests/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lagomorpha;
+using LagomorphaTests.Handlers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace LagomorphaTests
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<ProductsHandler>();
+            services.AddLagomorpha(config =>
+            {
+                config.UseRabbitMQ()
+                      .WithDefaultAssembly(typeof(Startup).Assembly)
+                      .WithHost("localhost")
+                      .WithPort(26001)
+                      .WithUsername("root")
+                      .WithPassword("!root");
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
